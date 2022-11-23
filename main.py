614a70d89b483dcf1772310f8b8106cfaffd2152
Matijast <matijastipcevic01@yahoo.com>
2022-11-23 17:22:32 +0000

    Clean up the code

--- main.py
+++ main.py
@@ -6,32 +6,23 @@ pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pdf.add_page()
-
-    # Set the header
-    pdf.set_font(family="Times", style="B", size=24)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-
-    # Set the footer
-    pdf.ln(266)
-    pdf.set_font(family="Times", style="I", size=8)
-    pdf.set_text_color(150, 150, 150)
-    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-
-    # Create lines for lined paper
-    for y in range(21, 298, 10):
-        pdf.line(10, y, 200, y)
-
-    for page in range(row["Pages"] - 1):
+    for page in range(row["Pages"]):
         pdf.add_page()
+        if page == 0:
+            # Set the header
+            pdf.set_font(family="Times", style="B", size=24)
+            pdf.set_text_color(100, 100, 100)
+            pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+            pdf.ln(266)
+        else:
+            pdf.ln(278)
 
         # Set the footer
-        pdf.ln(278)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(150, 150, 150)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+        # Create lines for lined paper
         for y in range(21, 298, 10):
             pdf.line(10, y, 200, y)
 
